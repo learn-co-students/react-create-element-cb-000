22fed111515c54ce91bae79f87989c556260b366
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-02-04 21:28:59 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const title = React.createElement('h1', {}, 'My First React Code');
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+const container = React.createElement('div', {}, [title, paragraph]);
 const list = 
   React.createElement('div', {},
     React.createElement('h1', {}, 'My favor ice cream flavors'),
